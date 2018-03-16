4c6b1aa679ea13a2a60757266dabd9ffb4ee71ca
Ricardo Ambrogi <ricardo.ambrogi@elements.nl>
2018-03-16 14:33:53 +0100

    defaultval

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ export class Machine extends React.Component {
 	constructor(props) {
 		super(props);
 		context.addFlow(props.state.flow);
-		console.log(context.data._defaultValue);
 
 		this.state = {
 			route: context.data._defaultValue,
