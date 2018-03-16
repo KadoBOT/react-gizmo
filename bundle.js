a2c32698dfaff32c11b13747cf407c40ce702d99
Ricardo Ambrogi <ricardo.ambrogi@elements.nl>
2018-03-16 13:36:37 +0100

    removed props

--- bundle.js
+++ bundle.js
@@ -211,321 +211,320 @@ var css = "* {\n  box-sizing: border-box;\n  position: relative; }\n\nhtml,\nbod
 __$$styleInject(css);
 
 var Context = function () {
-  function Context(flow) {
-    _classCallCheck(this, Context);
+	function Context(flow) {
+		_classCallCheck(this, Context);
 
-    this.machine = null;
-    this.data = null;
-    this.children = new _Map();
-  }
+		this.machine = null;
+		this.data = null;
+		this.children = new _Map();
+	}
 
-  _createClass(Context, [{
-    key: "addFlow",
-    value: function addFlow(flow) {
-      this.machine = xstate.Machine(flow);
-      this.data = React.createContext(this.machine.initialState.value);
-    }
-  }, {
-    key: "setChild",
-    value: function setChild(child, name) {
-      child && this.children.set(name, child);
-    }
-  }]);
+	_createClass(Context, [{
+		key: "addFlow",
+		value: function addFlow(flow) {
+			this.machine = xstate.Machine(flow);
+			this.data = React.createContext(this.machine.initialState.value);
+		}
+	}, {
+		key: "setChild",
+		value: function setChild(child, name) {
+			child && this.children.set(name, child);
+		}
+	}]);
 
-  return Context;
+	return Context;
 }();
 
 var context = new Context();
 
 var Machine = function (_React$Component) {
-  _inherits(Machine, _React$Component);
+	_inherits(Machine, _React$Component);
 
-  function Machine(props) {
-    var _this2 = this;
+	function Machine(props) {
+		var _this2 = this;
 
-    _classCallCheck(this, Machine);
+		_classCallCheck(this, Machine);
 
-    var _this = _possibleConstructorReturn(this, (Machine.__proto__ || _Object$getPrototypeOf(Machine)).call(this, props));
+		var _this = _possibleConstructorReturn(this, (Machine.__proto__ || _Object$getPrototypeOf(Machine)).call(this, props));
 
-    _this.transition = function (_ref) {
-      var to = _ref.to,
-          _ref$off = _ref.off,
-          off = _ref$off === undefined ? "" : _ref$off,
-          setState = _ref.setState,
-          draftState = _ref.draftState,
-          condition = _ref.condition;
+		_this.transition = function (_ref) {
+			var to = _ref.to,
+			    _ref$off = _ref.off,
+			    off = _ref$off === undefined ? "" : _ref$off,
+			    setState = _ref.setState,
+			    draftState = _ref.draftState,
+			    condition = _ref.condition;
 
-      _this.props.log && console.log({
-        on: _this.state.route,
-        to: to,
-        setState: setState,
-        draftState: draftState,
-        condition: condition
-      });
+			_this.props.log && console.log({
+				on: _this.state.route,
+				to: to,
+				setState: setState,
+				draftState: draftState,
+				condition: condition
+			});
 
-      _this.setState({
-        graph: {
-          on: _this.state.route
-        }
-      });
+			_this.setState({
+				graph: {
+					on: _this.state.route
+				}
+			});
 
-      var nextState = context.machine.transition(_this.state.route, to, condition);
-      var doAction = function doAction(_ref2) {
-        var route = _ref2.route;
+			var nextState = context.machine.transition(_this.state.route, to, condition);
 
-        if (context.children.has(route) && _this.state.flow.has(_this.state.route)) {
-          var _iteratorNormalCompletion = true;
-          var _didIteratorError = false;
-          var _iteratorError = undefined;
+			var doAction = function doAction(_ref2) {
+				var route = _ref2.route;
 
-          try {
-            for (var _iterator = _getIterator(nextState.actions), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-              var action = _step.value;
+				if (context.children.has(route) && _this.state.flow.has(_this.state.route)) {
+					var _iteratorNormalCompletion = true;
+					var _didIteratorError = false;
+					var _iteratorError = undefined;
 
-              context.children.get(route)[action] && context.children.get(route)[action]();
-            }
-          } catch (err) {
-            _didIteratorError = true;
-            _iteratorError = err;
-          } finally {
-            try {
-              if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-              }
-            } finally {
-              if (_didIteratorError) {
-                throw _iteratorError;
-              }
-            }
-          }
-        }
-      };
+					try {
+						for (var _iterator = _getIterator(nextState.actions), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+							var action = _step.value;
 
-      doAction({ route: _this.state.route });
+							context.children.get(route)[action] && context.children.get(route)[action]();
+						}
+					} catch (err) {
+						_didIteratorError = true;
+						_iteratorError = err;
+					} finally {
+						try {
+							if (!_iteratorNormalCompletion && _iterator.return) {
+								_iterator.return();
+							}
+						} finally {
+							if (_didIteratorError) {
+								throw _iteratorError;
+							}
+						}
+					}
+				}
+			};
 
-      var filteredFlow = _this.state.flow.add(nextState.value);
+			doAction({ route: _this.state.route });
 
-      if ((typeof off === "undefined" ? "undefined" : _typeof(off)) === "object") {
-        var _iteratorNormalCompletion2 = true;
-        var _didIteratorError2 = false;
-        var _iteratorError2 = undefined;
+			var filteredFlow = _this.state.flow.add(nextState.value);
 
-        try {
-          for (var _iterator2 = _getIterator(off), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var _flow = _step2.value;
-            filteredFlow.delete(_flow);
-          }
-        } catch (err) {
-          _didIteratorError2 = true;
-          _iteratorError2 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-              _iterator2.return();
-            }
-          } finally {
-            if (_didIteratorError2) {
-              throw _iteratorError2;
-            }
-          }
-        }
-      } else filteredFlow.delete(off);
+			if ((typeof off === "undefined" ? "undefined" : _typeof(off)) === "object") {
+				var _iteratorNormalCompletion2 = true;
+				var _didIteratorError2 = false;
+				var _iteratorError2 = undefined;
 
-      _this.setState(function (state) {
-        return {
-          route: nextState.value,
-          flow: filteredFlow,
-          state: _extends({}, state.state, setState),
-          draftState: draftState,
-          graph: {
-            on: state.route,
-            to: nextState.value
-          }
-        };
-      }, function () {
-        return doAction({ route: nextState.value });
-      });
-    };
+				try {
+					for (var _iterator2 = _getIterator(off), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+						var _flow = _step2.value;
+						filteredFlow.delete(_flow);
+					}
+				} catch (err) {
+					_didIteratorError2 = true;
+					_iteratorError2 = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion2 && _iterator2.return) {
+							_iterator2.return();
+						}
+					} finally {
+						if (_didIteratorError2) {
+							throw _iteratorError2;
+						}
+					}
+				}
+			} else filteredFlow.delete(off);
 
-    _this.publish = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
-      return _regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              _context.next = 2;
-              return _this.transition;
+			_this.setState(function (state) {
+				return {
+					route: nextState.value,
+					flow: filteredFlow,
+					state: _extends({}, state.state, setState),
+					draftState: draftState,
+					graph: {
+						on: state.route,
+						to: nextState.value
+					}
+				};
+			}, function () {
+				return doAction({ route: nextState.value });
+			});
+		};
 
-            case 2:
-              _this.setState(function (state) {
-                return {
-                  state: _extends({}, state.state, state.draftState),
-                  draftState: null
-                };
-              });
+		_this.publish = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
+			return _regeneratorRuntime.wrap(function _callee$(_context) {
+				while (1) {
+					switch (_context.prev = _context.next) {
+						case 0:
+							_context.next = 2;
+							return _this.transition;
 
-            case 3:
-            case "end":
-              return _context.stop();
-          }
-        }
-      }, _callee, _this2);
-    }));
+						case 2:
+							_this.setState(function (state) {
+								return {
+									state: _extends({}, state.state, state.draftState),
+									draftState: null
+								};
+							});
 
-    context.addFlow(props.state.flow);
+						case 3:
+						case "end":
+							return _context.stop();
+					}
+				}
+			}, _callee, _this2);
+		}));
 
-    _this.state = {
-      route: context.data.defaultValue,
-      flow: new _Set([context.machine.initialState.value]),
-      transition: _this.transition,
-      state: _this.props.state.initialState,
-      draftState: null,
-      publish: _this.publish,
-      graph: {
-        on: context.data.defaultValue,
-        to: ""
-      }
-    };
-    return _this;
-  }
+		context.addFlow(props.state.flow);
 
-  _createClass(Machine, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      if (context.children.has(this.state.route)) {
-        var _iteratorNormalCompletion3 = true;
-        var _didIteratorError3 = false;
-        var _iteratorError3 = undefined;
+		_this.state = {
+			route: context.data.defaultValue,
+			flow: new _Set([context.machine.initialState.value]),
+			transition: _this.transition,
+			state: _this.props.state.initialState,
+			draftState: null,
+			publish: _this.publish,
+			graph: {
+				on: context.data.defaultValue,
+				to: ""
+			}
+		};
+		return _this;
+	}
 
-        try {
-          for (var _iterator3 = _getIterator(context.machine.initialState.actions), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-            var action = _step3.value;
+	_createClass(Machine, [{
+		key: "componentDidMount",
+		value: function componentDidMount() {
+			if (context.children.has(this.state.route)) {
+				var _iteratorNormalCompletion3 = true;
+				var _didIteratorError3 = false;
+				var _iteratorError3 = undefined;
 
-            context.children.get(this.state.route)[action] && context.children.get(this.state.route)[action]();
-          }
-        } catch (err) {
-          _didIteratorError3 = true;
-          _iteratorError3 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion3 && _iterator3.return) {
-              _iterator3.return();
-            }
-          } finally {
-            if (_didIteratorError3) {
-              throw _iteratorError3;
-            }
-          }
-        }
-      }
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var Provider = context.data.Provider;
+				try {
+					for (var _iterator3 = _getIterator(context.machine.initialState.actions), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+						var action = _step3.value;
+
+						context.children.get(this.state.route)[action] && context.children.get(this.state.route)[action]();
+					}
+				} catch (err) {
+					_didIteratorError3 = true;
+					_iteratorError3 = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion3 && _iterator3.return) {
+							_iterator3.return();
+						}
+					} finally {
+						if (_didIteratorError3) {
+							throw _iteratorError3;
+						}
+					}
+				}
+			}
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			var Provider = context.data.Provider;
 
 
-      return this.props.graph ? React.createElement(
-        "div",
-        { className: "gizmo__graph" },
-        React.createElement(Graph, { flow: this.state.graph, machine: this.props.state.flow }),
-        React.createElement(
-          "div",
-          { className: "gizmo__wrapper" },
-          React.createElement(
-            Provider,
-            { value: this.state },
-            React.cloneElement(this.props.children, {
-              route: this.state.route
-            })
-          )
-        )
-      ) : React.createElement(
-        Provider,
-        { value: this.state },
-        React.cloneElement(this.props.children, {
-          route: this.state.route
-        })
-      );
-    }
-  }]);
+			return this.props.graph ? React.createElement(
+				"div",
+				{ className: "gizmo__graph" },
+				React.createElement(Graph, {
+					flow: this.state.graph,
+					machine: this.props.state.flow
+				}),
+				React.createElement(
+					"div",
+					{ className: "gizmo__wrapper" },
+					React.createElement(
+						Provider,
+						{ value: this.state },
+						React.cloneElement(this.props.children, {
+							route: this.state.route
+						})
+					)
+				)
+			) : React.createElement(
+				Provider,
+				{ value: this.state },
+				React.cloneElement(this.props.children, {
+					route: this.state.route
+				})
+			);
+		}
+	}]);
 
-  return Machine;
+	return Machine;
 }(React.Component);
 
 var State = function (_React$Component2) {
-  _inherits(State, _React$Component2);
+	_inherits(State, _React$Component2);
 
-  function State() {
-    var _ref4;
+	function State() {
+		var _ref4;
 
-    var _temp, _this3, _ret;
+		var _temp, _this3, _ret;
 
-    _classCallCheck(this, State);
+		_classCallCheck(this, State);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+		for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+			args[_key] = arguments[_key];
+		}
 
-    return _ret = (_temp = (_this3 = _possibleConstructorReturn(this, (_ref4 = State.__proto__ || _Object$getPrototypeOf(State)).call.apply(_ref4, [this].concat(args))), _this3), _initialiseProps.call(_this3), _temp), _possibleConstructorReturn(_this3, _ret);
-  }
+		return _ret = (_temp = (_this3 = _possibleConstructorReturn(this, (_ref4 = State.__proto__ || _Object$getPrototypeOf(State)).call.apply(_ref4, [this].concat(args))), _this3), _this3.checkType = function (children, _ref5) {
+			var _transition = _ref5.transition,
+			    publish = _ref5.publish,
+			    state = _ref5.state;
 
-  _createClass(State, [{
-    key: "render",
-    value: function render() {
-      var _this4 = this;
+			var props = _extends({
+				transition: function transition(to, options, cond) {
+					return _transition(_extends({ to: to }, options), cond);
+				},
+				publish: publish
+			}, state);
+			if (children.type.prototype && children.type.prototype.isReactComponent) {
+				return React.cloneElement(children, _extends({
+					ref: function ref(node) {
+						return context.setChild(node, _this3.props.on);
+					}
+				}, props));
+			} else {
+				console.warn("<State /> should not return stateless functions on Children:", children);
+				return React.cloneElement(children, _extends({}, props));
+			}
+		}, _temp), _possibleConstructorReturn(_this3, _ret);
+	}
 
-      var Consumer = context.data.Consumer;
+	_createClass(State, [{
+		key: "render",
+		value: function render() {
+			var _this4 = this;
+
+			var Consumer = context.data.Consumer;
 
 
-      return React.createElement(
-        Consumer,
-        null,
-        function (_ref5) {
-          var flow = _ref5.flow,
-              transition = _ref5.transition,
-              publish = _ref5.publish,
-              state = _ref5.state;
+			return React.createElement(
+				Consumer,
+				null,
+				function (_ref6) {
+					var flow = _ref6.flow,
+					    transition = _ref6.transition,
+					    publish = _ref6.publish,
+					    state = _ref6.state;
 
-          var render = _this4.checkType(_this4.props.render, {
-            transition: transition,
-            state: state,
-            publish: publish
-          });
-          return flow.has(_this4.props.on) && render;
-        }
-      );
-    }
-  }]);
+					var children = _this4.checkType(_this4.props.children, {
+						transition: transition,
+						state: state,
+						publish: publish
+					});
 
-  return State;
+					return flow.has(_this4.props.on) && children;
+				}
+			);
+		}
+	}]);
+
+	return State;
 }(React.Component);
-
-var _initialiseProps = function _initialiseProps() {
-  var _this5 = this;
-
-  this.checkType = function (fn, _ref6) {
-    var _transition = _ref6.transition,
-        publish = _ref6.publish,
-        state = _ref6.state;
-
-    var args = _extends({
-      transition: function transition(to, options, cond) {
-        return _transition(_extends({ to: to }, options), cond);
-      },
-      publish: publish
-    }, state);
-    var render = _extends({}, fn(args));
-    if (render.type.prototype && render.type.prototype.isReactComponent) {
-      render.ref = function (node) {
-        return context.setChild(node, _this5.props.on);
-      };
-    } else {
-      console.warn("<State /> should not return stateless functions on render:", fn(args));
-    }
-
-    return render;
-  };
-};
 
 exports.Machine = Machine;
 exports.State = State;
--- src/index.js
+++ src/index.js
@@ -5,170 +5,194 @@ import Graph from "./Graph";
 import "./graph.scss";
 
 class Context {
-  constructor(flow) {
-    this.machine = null;
-    this.data = null;
-    this.children = new Map();
-  }
+	constructor(flow) {
+		this.machine = null;
+		this.data = null;
+		this.children = new Map();
+	}
 
-  addFlow(flow) {
-    this.machine = Xstate(flow);
-    this.data = React.createContext(this.machine.initialState.value);
-  }
+	addFlow(flow) {
+		this.machine = Xstate(flow);
+		this.data = React.createContext(this.machine.initialState.value);
+	}
 
-  setChild(child, name) {
-    child && this.children.set(name, child);
-  }
+	setChild(child, name) {
+		child && this.children.set(name, child);
+	}
 }
 
 let context = new Context();
 
 export class Machine extends React.Component {
-  constructor(props) {
-    super(props);
-    context.addFlow(props.state.flow);
+	constructor(props) {
+		super(props);
+		context.addFlow(props.state.flow);
 
-    this.state = {
-      route: context.data.defaultValue,
-      flow: new Set([context.machine.initialState.value]),
-      transition: this.transition,
-      state: this.props.state.initialState,
-      draftState: null,
-      publish: this.publish,
-      graph: {
-        on: context.data.defaultValue,
-        to: ""
-      }
-    };
-  }
+		this.state = {
+			route: context.data.defaultValue,
+			flow: new Set([context.machine.initialState.value]),
+			transition: this.transition,
+			state: this.props.state.initialState,
+			draftState: null,
+			publish: this.publish,
+			graph: {
+				on: context.data.defaultValue,
+				to: ""
+			}
+		};
+	}
 
-  componentDidMount() {
-    if (context.children.has(this.state.route)) {
-      for (const action of context.machine.initialState.actions) {
-        context.children.get(this.state.route)[action] &&
-          context.children.get(this.state.route)[action]();
-      }
-    }
-  }
+	componentDidMount() {
+		if (context.children.has(this.state.route)) {
+			for (const action of context.machine.initialState.actions) {
+				context.children.get(this.state.route)[action] &&
+					context.children.get(this.state.route)[action]();
+			}
+		}
+	}
 
-  transition = ({ to, off = "", setState, draftState, condition }) => {
-    this.props.log &&
-      console.log({
-        on: this.state.route,
-        to,
-        setState,
-        draftState,
-        condition
-      });
+	transition = ({ to, off = "", setState, draftState, condition }) => {
+		this.props.log &&
+			console.log({
+				on: this.state.route,
+				to,
+				setState,
+				draftState,
+				condition
+			});
 
-    this.setState({
-      graph: {
-        on: this.state.route
-      }
-    });
+		this.setState({
+			graph: {
+				on: this.state.route
+			}
+		});
 
-    let nextState = context.machine.transition(this.state.route, to, condition);
-    const doAction = ({ route }) => {
-      if (
-        context.children.has(route) &&
-        this.state.flow.has(this.state.route)
-      ) {
-        for (const action of nextState.actions) {
-          context.children.get(route)[action] &&
-            context.children.get(route)[action]();
-        }
-      }
-    };
+		let nextState = context.machine.transition(
+			this.state.route,
+			to,
+			condition
+		);
 
-    doAction({ route: this.state.route });
+		const doAction = ({ route }) => {
+			if (
+				context.children.has(route) &&
+				this.state.flow.has(this.state.route)
+			) {
+				for (const action of nextState.actions) {
+					context.children.get(route)[action] &&
+						context.children.get(route)[action]();
+				}
+			}
+		};
 
-    let filteredFlow = this.state.flow.add(nextState.value);
+		doAction({ route: this.state.route });
 
-    if (typeof off === "object")
-      for (const flow of off) filteredFlow.delete(flow);
-    else filteredFlow.delete(off);
+		let filteredFlow = this.state.flow.add(nextState.value);
 
-    this.setState(
-      state => ({
-        route: nextState.value,
-        flow: filteredFlow,
-        state: { ...state.state, ...setState },
-        draftState,
-        graph: {
-          on: state.route,
-          to: nextState.value
-        }
-      }),
-      () => doAction({ route: nextState.value })
-    );
-  };
+		if (typeof off === "object")
+			for (const flow of off) filteredFlow.delete(flow);
+		else filteredFlow.delete(off);
 
-  publish = async () => {
-    await this.transition;
-    this.setState(state => ({
-      state: { ...state.state, ...state.draftState },
-      draftState: null
-    }));
-  };
+		this.setState(
+			state => ({
+				route: nextState.value,
+				flow: filteredFlow,
+				state: { ...state.state, ...setState },
+				draftState,
+				graph: {
+					on: state.route,
+					to: nextState.value
+				}
+			}),
+			() => doAction({ route: nextState.value })
+		);
+	};
 
-  render() {
-    const { Provider } = context.data;
+	publish = async () => {
+		await this.transition;
+		this.setState(state => ({
+			state: { ...state.state, ...state.draftState },
+			draftState: null
+		}));
+	};
 
-    return this.props.graph ? (
-      <div className="gizmo__graph">
-        <Graph flow={this.state.graph} machine={this.props.state.flow} />
-        <div className="gizmo__wrapper">
-          <Provider value={this.state}>
-            {React.cloneElement(this.props.children, {
-              route: this.state.route
-            })}
-          </Provider>
-        </div>
-      </div>
-    ) : (
-      <Provider value={this.state}>
-        {React.cloneElement(this.props.children, {
-          route: this.state.route
-        })}
-      </Provider>
-    );
-  }
+	render() {
+		const { Provider } = context.data;
+
+		return this.props.graph ? (
+			<div className="gizmo__graph">
+				<Graph
+					flow={this.state.graph}
+					machine={this.props.state.flow}
+				/>
+				<div className="gizmo__wrapper">
+					<Provider value={this.state}>
+						{React.cloneElement(this.props.children, {
+							route: this.state.route
+						})}
+					</Provider>
+				</div>
+			</div>
+		) : (
+			<Provider value={this.state}>
+				{React.cloneElement(this.props.children, {
+					route: this.state.route
+				})}
+			</Provider>
+		);
+	}
 }
 
 export class State extends React.Component {
-  checkType = (fn, { transition, publish, state }) => {
-    let args = {
-      transition: (to, options, cond) => transition({ to, ...options }, cond),
-      publish,
-      ...state
-    };
-    const render = { ...fn(args) };
-    if (render.type.prototype && render.type.prototype.isReactComponent) {
-      render.ref = node => context.setChild(node, this.props.on);
-    } else {
-      console.warn(
-        "<State /> should not return stateless functions on render:",
-        fn(args)
-      );
-    }
+	checkType = (children, { transition, publish, state }) => {
+		let props = {
+			transition: (to, options, cond) =>
+				transition({ to, ...options }, cond),
+			publish,
+			...state
+		};
+		if (
+			children.type.prototype &&
+			children.type.prototype.isReactComponent
+		) {
+			return React.cloneElement(children, {
+				ref: node => context.setChild(node, this.props.on),
+				...props
+			});
+		} else {
+			console.warn(
+				"<State /> should not return stateless functions on Children:",
+				children
+			);
+			return React.cloneElement(children, {
+				...props
+			});
+		}
+	};
 
-    return render;
-  };
+	render() {
+		const { Consumer } = context.data;
 
-  render() {
-    const { Consumer } = context.data;
-
-    return (
-      <Consumer>
-        {({ flow, transition, publish, state }) => {
+		return (
+			<Consumer>
+				{/* ({ flow, transition, publish, state }) => {
           const render = this.checkType(this.props.render, {
             transition,
             state,
             publish
           });
           return flow.has(this.props.on) && render;
-        }}
-      </Consumer>
-    );
-  }
+        } */}
+				{({ flow, transition, publish, state }) => {
+					const children = this.checkType(this.props.children, {
+						transition,
+						state,
+						publish
+					});
+
+					return flow.has(this.props.on) && children;
+				}}
+			</Consumer>
+		);
+	}
 }
